5a86d28ac4a79c695dfb183ae51cd4d53c0025bb
SnippyRex <kentf022@gmail.com>
2023-02-24 20:08:05 +0000

    Fix: Add defer attribute and append element directly to HTML body

--- UI.js
+++ UI.js
@@ -1,23 +1,22 @@
 
-//? Tried to add the button container as a div and give it an ID instead of making the div in the HTML body but didn't seem to work 
-//const buttonContainer = document.createElement('div')
-//buttonContainer.setAttribute('id', 'buttonContainer');
-
-
-const container = document.getElementById('btnContainer');
+const container = document.createElement('div');
+container.setAttribute('id', 'btnContainer');
+document.body.appendChild(container);
 
 const button = document.createElement('button');
-/*button.textContent = 'Rock';
+button.textContent = 'Rock';
 button.style.fontSize = '20px';
-button.classList.add('rock');*/
+button.classList.add('rock');
 
 const button1 = document.createElement('button');
-/*button1.textContent = 'Paper';
-button1.classList.add('paper');*/
+button1.textContent = 'Paper';
+button1.style.fontSize = '20px';
+button1.classList.add('paper');
 
 const button2 = document.createElement('button');
-/*button2.textContent = 'Scissors';
-button2.classList.add('scissors');*/
+button2.textContent = 'Scissors';
+button2.style.fontSize = '20px';
+button2.classList.add('scissors');
 
 container.appendChild(button);
 container.appendChild(button1);
